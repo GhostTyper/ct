2b60638f6433a5d665604a71bf6eb763d729cb7d
GhostTyper <matthias@lukaseder.de>
2025-05-23 00:27:07 +0200

    Add virtual message stream method

--- ct/ct/DataSource.cs
+++ ct/ct/DataSource.cs
@@ -12,5 +12,16 @@ namespace ct
         /// Specifies if the data source is live (data from an exchange) or from a offline file for back-testing.
         /// </summary>
         public virtual bool Live => false;
+
+        /// <summary>
+        /// Retrieves messages from the data source. Implementations typically
+        /// yield messages endlessly from an exchange or a file.
+        /// </summary>
+        /// <returns>An asynchronous stream of <see cref="Message"/> objects.</returns>
+        public virtual async IAsyncEnumerable<Message> GetMessagesAsync()
+        {
+            await Task.CompletedTask;
+            yield break;
+        }
     }
 }

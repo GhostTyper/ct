97601a9e8dc9f5fbaac89654139fa86185570d28
GhostTyper <matthias@lukaseder.de>
2025-05-23 03:04:55 +0200

    Group same timestamp trades and track count.

--- ct/ct/TradeMessage.cs
+++ ct/ct/TradeMessage.cs
@@ -14,19 +14,27 @@ namespace ct
         public readonly double Volume;
         public readonly TradeDirection Direction;
 
-        public TradeMessage(DateTime stamp, double startPrice, double endPrice, double volume, TradeDirection direction) : base(stamp)
+        private int _count;
+        public int Count => _count;
+
+        public TradeMessage(DateTime stamp, double startPrice, double endPrice, double volume, TradeDirection direction) : this(stamp, startPrice, endPrice, volume, direction, 1)
+        {
+        }
+
+        public TradeMessage(DateTime stamp, double startPrice, double endPrice, double volume, TradeDirection direction, int count) : base(stamp)
         {
             StartPrice = startPrice;
             EndPrice = endPrice;
             Direction = direction;
             Volume = volume;
+            _count = count;
         }
 
         public override MessageKind Kind => MessageKind.Trade;
 
         public override string ToString()
         {
-            return $"Trade {Stamp:O} start={StartPrice} end={EndPrice} volume={Volume} direction={Direction}";
+            return $"Trade {Stamp:O} start={StartPrice} end={EndPrice} volume={Volume} direction={Direction} count={Count}";
         }
     }
 }
